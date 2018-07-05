0b1c16199a0748c5fa05176160aa1a8371c482e5
David Buchan-Swanson <david.buchanswanson@gmail.com>
2018-07-05 11:59:45 +1000

    fix up imports in extract

--- src/extract.js
+++ src/extract.js
@@ -1,11 +1,10 @@
 /* @flow */
 
+import mkdirp from 'mkdirp';
 import type { Options } from './util/retrieveDefaultConfig';
-import { debug, success, log, warn } from './util/debugLog';
+import { debug, success, log } from './util/debugLog';
 import defaults from './util/retrieveDefaultConfig';
 import extractTranslations from './util/extractTranslations';
-import createRequester from './util/request';
-import parse from './util/parsePO';
 
 export default async function sync(options: Options): Promise<void> {
   log('Setting everything up...');
@@ -14,7 +13,6 @@ export default async function sync(options: Options): Promise<void> {
   if (opts.debug) {
     debug('Loaded options', JSON.stringify(opts, null, 2));
   }
-  const req = createRequester(opts);
   success('Setup Complete.');
   log('Extracting new translations...');
   await extractTranslations(
